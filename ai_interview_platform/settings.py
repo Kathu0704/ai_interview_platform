61504b12d80c175026a2f3a278ae1f4ebc4d4be6
kathu0704 <karthikspoojary07@gmail.com>
2026-02-10 09:01:29 +0530

    changing the data base base (sqlite to postgreSQL

--- ai_interview_platform/settings.py
+++ ai_interview_platform/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,11 +86,14 @@ WSGI_APPLICATION = 'ai_interview_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+default_sqlite_url = f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL", default_sqlite_url),
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
 # OTP Configuration
